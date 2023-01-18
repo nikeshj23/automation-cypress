b685d711a98aed47ecb79f4fb34a9623870f8fe4
nikeshj23 <jonnanikesh2398@gmail.com>
2023-01-18 16:49:07 +0530

    feat: :art: End to End Automation Testing

--- cypress/integration/smoke-test.spec.js
+++ cypress/integration/smoke-test.spec.js
@@ -51,18 +51,30 @@ describe("smoke tests", () => {
         })
         .should("not.exist");
     });
-  it.only("Toggles todos", () => {
+  it("Toggles todos", () => {
+    const clickAndWait =($el) => {
+        cy.wrap($el)
+        .as("item")
+        .find(".toggle")
+        .click()
+  
+        cy.wait("@update")
+    }
     cy.server()
-    cy.route("PUT", "/api/todos/*").as("update")
-
-    cy.get(".todo-list li").each(($el) => {
-      cy.wrap($el).as("item").find(".toggle").click()
-
-      cy.wait("@update")
-
-      cy.get("@item").should("have.class", "completed")
+    cy.route("PUT", "/api/todos/*")
+    .as("update")
+ 
+    cy.get(".todo-list li")
+    .each(($el) => {
+      clickAndWait($el)
+      cy.get("@item")
+      .should("have.class", "completed")
     })
+       .each(($el) => {
+      clickAndWait($el)
+      cy.get("@item")
+      .should("not.have.class", "completed")
   })
 })
 })
-
+})
